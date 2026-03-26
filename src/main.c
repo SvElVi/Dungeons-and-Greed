fe3ed96cff2c052d6ac27e0dbcea58a775be0632
hiddenname <nils.gjardman@gmail.com>
2026-03-26 17:03:34 +0100

    Added main.c
    The beginnings of a program.

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#define SDL_MAIN_USE_CALLBACKS 1 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    if (!SDL_CreateWindowAndRenderer("GameWindow", 1920, 1080, SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED, &window, &renderer)) {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_KEY_DOWN ||
+        event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+}

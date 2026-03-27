a6b1490f92cacb7b00e57201ae9e606af074c5b5
hiddenname <nils.gjardman@gmail.com>
2026-03-27 15:14:29 +0100

    Reveted initdisplay() function

--- src/main.c
+++ src/main.c
@@ -10,7 +10,16 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) //Runs once a
     AppState* state = (AppState*)SDL_calloc(1, sizeof(AppState)); //Create space on heap
     if(!state) return SDL_APP_FAILURE;
 
-    if(initDisplay(state) == SDL_APP_FAILURE) return SDL_APP_FAILURE;
+    if(!(state->displayID = SDL_GetPrimaryDisplay())) {
+        SDL_Log("Failed getting DisplayID: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    } else if(!SDL_GetDisplayBounds(state->displayID, &(state->displaySize))) {
+        SDL_Log("Failed getting window bounds: %", SDL_GetError());
+        return SDL_APP_FAILURE;
+    } else if (!SDL_CreateWindowAndRenderer("GameWindow", state->displaySize.w, state->displaySize.h, SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED, &(state->window), &(state->renderer))) {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
 
     state->running = true; //Custom flag to mark the program as running
 

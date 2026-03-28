1270871ef79ebd378a75a5ab4b44cb7cf359f348
hiddenname <nils.gjardman@gmail.com>
2026-03-28 16:29:35 +0100

    Update inits.h
    Added player struct and components

--- src/inits.h
+++ src/inits.h
@@ -1,3 +1,31 @@
+typedef enum {
+    CLASS_NONE, //0
+    CLASS_MAGE,
+    CLASS_PRIEST,
+    CLASS_HUNTER,
+    CLASS_SWORDMASTER,
+    CLASS_KNIGHT //5
+} Player_Class;
+
+typedef struct {
+    int x;
+    int y;
+} Vector2D;
+
+typedef struct {
+    int health;
+    int mana;
+    int defense;
+    int attackPower;
+    int attackSpeed;
+} Stats;
+
+typedef struct {
+    Vector2D pos;
+    Player_Class class;
+    Stats stats;
+} Player;
+
 typedef struct {
     SDL_Window *window;
     SDL_Renderer *renderer;

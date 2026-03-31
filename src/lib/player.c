8c50c8df91c36c34a3c44a5f84d7b2235cb9c406
hiddenname <nils.gjardman@gmail.com>
2026-03-31 13:52:07 +0200

    updatePlayer() completed
    Not yet fully tested, but implemented. Does not cause crashes.

--- src/lib/player.c
+++ src/lib/player.c
@@ -6,3 +6,36 @@ void movement(Player* player, int deltatime) {
     if(player->flags.moveY != 0) player->pos.y += player->flags.moveY * deltatime * SPEED;
     // SDL_Log("PosX: %d PosY: %d", player->pos.x, player->pos.y);
 }
+
+void updateClass(Player* player, SDL_Renderer* renderer) {
+    SDL_Surface* pArt;
+    switch (player->class) { //Paths below should be change in the future
+        case CLASS_NONE:
+            pArt = SDL_LoadPNG("./img/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png");
+            break;
+        case CLASS_MAGE:
+            pArt = SDL_LoadPNG("./img/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png");
+            break;
+        case CLASS_PRIEST:
+            pArt = SDL_LoadPNG("./img/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png");
+            break;
+        case CLASS_HUNTER:
+            pArt = SDL_LoadPNG("./img/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png");
+            break;
+        case CLASS_SWORDMASTER:
+            pArt = SDL_LoadPNG("./img/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png");
+            break;
+        case CLASS_KNIGHT:
+            pArt = SDL_LoadPNG("./img/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png");
+            break;
+    }
+    player->texture = SDL_CreateTextureFromSurface(renderer, pArt);
+    SDL_DestroySurface(pArt);
+}
+
+void updatePlayer(Player* players, int plNb, Vector2D pos, Player_Class class, Stats stats, SDL_Renderer* renderer) {
+    if(&pos) players[plNb].pos = pos;
+    if(&class) players[plNb].class = class;
+    updateClass(&(players[plNb]), renderer);
+    if(&stats) players[plNb].stats = stats;
+}
--- src/lib/player.h
+++ src/lib/player.h
@@ -1,3 +1,5 @@
 #include "render.h"
 
 void movement(Player* player, int deltatime);
+
+void updatePlayer(int plNb, Vector2D pos, Player_Class class, Stats stats, SDL_Renderer* renderer);

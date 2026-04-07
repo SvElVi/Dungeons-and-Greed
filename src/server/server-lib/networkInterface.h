ef679128c99635f2ea40e25833445dc7dd05361b
alehur-0 <alehur@kth.se>
2026-04-07 12:11:52 +0200

    Net update

--- src/server/server-lib/networkInterface.h
+++ src/server/server-lib/networkInterface.h
@@ -1,5 +1,9 @@
 #include <SDL3_Net/SDL_net.h>
+#define ADDRESS_LENGTH 17
 
 int startSDLNet(void);
 NET_DatagramSocket* createUDPSocket(int);
 void destoryUDPSocket(NET_DatagramSocket* udpSocket);
+NET_Server* createServerSocket(int portNumber);
+void destoryServerSocket(NET_Server*);
+void initAdress(NET_Address *adress, char ipAdress[ADDRESS_LENGTH]);

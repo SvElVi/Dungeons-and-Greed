4ae2ccc3867eeb9f3a0b8c5413fbd1680c862f9b
Jerry <wndehd17@yahoo.com>
2026-04-13 14:06:53 +0200

    Update player.c

--- src/lib/player.c
+++ src/lib/player.c
@@ -137,8 +137,8 @@ void updateClass(Player* player, SDL_Renderer* renderer) {
 void updatePlayer(Player* player, Vector2D pos, Player_Class class, Stats stats, SDL_Renderer* renderer) {
     player->aniBox.w = PLAYER_SIZE;
     player->aniBox.h = PLAYER_SIZE;
-    player->hitBox.w = 16;
-    player->hitBox.h = 5;
+    player->hitBox.w = 32;
+    player->hitBox.h = 16;
     player->aniBox.x = 0;
     player->aniBox.y = 0;
     player->pos = pos;

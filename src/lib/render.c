2eaade21a2aa4a9504fac1484f619794b56d9ba7
hiddenname <nils.gjardman@gmail.com>
2026-04-13 11:43:48 +0200

    Update render.c
    No longer renders players in main menu.

--- src/lib/render.c
+++ src/lib/render.c
@@ -2,45 +2,6 @@
 #include <string.h>
 
 int renderFrame(AppState state) {
-    SDL_FRect temp;
-    Vector2D tempV, renderOrder[MAX_PLAYERS];
-    SDL_SetRenderDrawColor(state->renderer,37,19,26,1);
-    SDL_RenderClear(state->renderer);
-
-    if(!renderDungeon(state)) {
-        return SDL_APP_FAILURE;
-    }
-
-    for(int j = 0; j < MAX_PLAYERS; j++) {
-        renderOrder[j].x = j;
-        renderOrder[j].y = state->players[j].pos.y;
-    }
-
-    for (int a = 0; a < MAX_PLAYERS -1; a++) {
-        for (int b = 0; b < MAX_PLAYERS -1 - a; b++) {
-            if (renderOrder[b].y < renderOrder[b+1].y) {
-                tempV = renderOrder[b];
-                renderOrder[b] = renderOrder[b+1];
-                renderOrder[b+1] = tempV;
-            }
-        }
-    }
-
-    for(int i = 0; i < MAX_PLAYERS; i++) {
-        if(state->players[renderOrder[i].x].texture) {
-
-            temp.h = PLAYER_SIZE*RENDER_SCALE;
-            temp.w = PLAYER_SIZE*RENDER_SCALE;
-            temp.x = state->camera.x + (state->players[0].pos.x - state->players[renderOrder[i].x].pos.x);
-            temp.y = state->camera.y + (state->players[0].pos.y - state->players[renderOrder[i].x].pos.y);
-
-            if(!(SDL_RenderTextureRotated(state->renderer, state->players[renderOrder[i].x].texture, &(state->players[renderOrder[i].x].aniBox), &(temp), 0, NULL, state->players[renderOrder[i].x].flip))) {
-                SDL_Log("FAILED RENDERING TEXTURE: %s", SDL_GetError());
-                return SDL_APP_FAILURE;
-            }
-        }
-    }
-
     if(state->gameState == GAME_START){
         const char *message = "Press SPACE to start";
         const char *title = "GREEDY DELVERS";
@@ -64,6 +25,45 @@ int renderFrame(AppState state) {
         SDL_RenderDebugText(state->renderer, x, y, message);
         SDL_RenderDebugText(state->renderer, titleX, titleY, title);
         SDL_SetRenderScale(state->renderer, 1.0f, 1.0f);
+    } else {
+        SDL_FRect temp;
+        Vector2D tempV, renderOrder[MAX_PLAYERS];
+        SDL_SetRenderDrawColor(state->renderer,37,19,26,1);
+        SDL_RenderClear(state->renderer);
+
+        if(!renderDungeon(state)) {
+            return SDL_APP_FAILURE;
+        }
+
+        for(int j = 0; j < MAX_PLAYERS; j++) {
+            renderOrder[j].x = j;
+            renderOrder[j].y = state->players[j].pos.y;
+        }
+
+        for (int a = 0; a < MAX_PLAYERS -1; a++) {
+            for (int b = 0; b < MAX_PLAYERS -1 - a; b++) {
+                if (renderOrder[b].y < renderOrder[b+1].y) {
+                    tempV = renderOrder[b];
+                    renderOrder[b] = renderOrder[b+1];
+                    renderOrder[b+1] = tempV;
+                }
+            }
+        }
+
+        for(int i = 0; i < MAX_PLAYERS; i++) {
+            if(state->players[renderOrder[i].x].texture) {
+
+                temp.h = PLAYER_SIZE*RENDER_SCALE;
+                temp.w = PLAYER_SIZE*RENDER_SCALE;
+                temp.x = state->camera.x + (state->players[0].pos.x - state->players[renderOrder[i].x].pos.x);
+                temp.y = state->camera.y + (state->players[0].pos.y - state->players[renderOrder[i].x].pos.y);
+
+                if(!(SDL_RenderTextureRotated(state->renderer, state->players[renderOrder[i].x].texture, &(state->players[renderOrder[i].x].aniBox), &(temp), 0, NULL, state->players[renderOrder[i].x].flip))) {
+                    SDL_Log("FAILED RENDERING TEXTURE: %s", SDL_GetError());
+                    return SDL_APP_FAILURE;
+                }
+            }
+        }
     }
 
     SDL_RenderPresent(state->renderer);

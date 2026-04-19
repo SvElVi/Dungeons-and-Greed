c40dd26bc125c76fabd542075f67cc6fec9fda50
Theodore <thhammarkth@gmail.com>
2026-04-19 21:08:34 +0200

    added functions to enemy.h

--- src/lib/enemy.h
+++ src/lib/enemy.h
@@ -5,5 +5,12 @@
 
 int nearestPlayer(Enemy* enemy, Player players[MAX_PLAYERS]);
 
+void enemyMovement(Enemy* enemy, Player players[MAX_PLAYERS], int deltatime);
+
+void animateEnemies(Enemy enemies[MAX_ENEMIES], Uint8* counter, Uint16 framerate);
+
+static void updateEnemyClass(Enemy* enemy, SDL_Renderer* renderer);
+
+void updateEnemy(Enemy* enemy, Vector2D pos, Enemy_Type type, Stats stats, SDL_Renderer* renderer);
 
 #endif

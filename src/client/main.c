96a9c5ca53647e9cc4f66fda45f6256dcc65af78
hiddenname <nils.gjardman@gmail.com>
2026-04-21 11:17:35 +0200

    Revert "Reapply "Merge branch 'dev' of https://github.com/SvElVi/Greedy-Delvers into dev""
    This reverts commit d7d98c8a1acb8e9808b25b0f889c51b43895a7c6.

--- src/client/main.c
+++ src/client/main.c
@@ -1,42 +1,39 @@
-#define SDL_MAIN_USE_CALLBACKS 1 // Flag to use callbacks
-#define SERVER_PORT 2000         // As of now... hardwired...
-#define CLIENT_PORT 2001         // As of now... hardwired...
+#define SDL_MAIN_USE_CALLBACKS 1 //Flag to use callbacks
+#define SERVER_PORT 2000 // As of now... hardwired...
+#define CLIENT_PORT 2001 // As of now... hardwired...
 #define DEBUG 1
 
 #include <SDL3/SDL_main.h>
 #include "../lib/NET/networkInterface.h"
 #include "client-lib/clientNet.h"
 #include "../lib/player.h" //All dependencies of [x] included
-#include "../lib/enemy.h"  //All dependencies of [x] included
 
 char ip[15] = "127.0.0.1";
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) // Runs once at the begining of the program
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) //Runs once at the begining of the program
 {
     SDL_Log("\n\n --------------- Starting Greedy-Delvers ---------------\n");
-    SDL_InitSubSystem(SDL_INIT_VIDEO); // Also initilizes appevents
+    SDL_InitSubSystem(SDL_INIT_VIDEO); //Also initilizes appevents
 
     AppState state = createAppState();
     state->gameState = GAME_INIT;
     state->mainMenu = (Menu){
         .menuOptions = {"Join", "Host", "Quit(current start)"},
         .selected = 0,
-        .count = 3};
-    if (!state)
-        return SDL_APP_FAILURE;
+        .count = 3
+    };
+    if(!state) return SDL_APP_FAILURE;
 
-    if (initDisplay(state))
-        return SDL_APP_FAILURE; // Initiate and display window
+    if(initDisplay(state)) return SDL_APP_FAILURE; //Initiate and display window
     initCam(state);
 
-    if (startSDLNet() == NET_FAILURE)
-        return SDL_APP_FAILURE;
+    if(startSDLNet() == NET_FAILURE) return SDL_APP_FAILURE;
 
     createUDPSocket(&state->udpSocket, SERVER_PORT);
 
     state->udpPacket = SDL_calloc(1, sizeof(NET_Datagram));
 
-    state->running = true; // Custom flag to mark the program as running
+    state->running = true; //Custom flag to mark the program as running
 
     Vector2D tempVec = {0, 0};
     Stats tempStats = {0};
@@ -51,11 +48,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) // Runs once 
     updatePlayer(&(state->players[2]), tempVec, CLASS_NONE, tempStats, state->renderer);
     SDL_strlcpy(state->players[2].name, "Player3", sizeof(state->players[2].name));
 
-    Vector2D enemyPos = {200, 100};
-    Stats enemyStats = {100, 0, 5, 10, 1};
-    updateEnemy(&state->enemies[0], enemyPos, ENEMY_SKELETON, enemyStats, state->renderer);
-
-    *appstate = state; // Share the appstate to callbacks below
+    *appstate = state; //Share the appstate to callbacks below
     // state->renderFlag = 1;
 
     state->world = createWorld(5, (Uint64)SDL_rand(0), state->renderer);
@@ -65,99 +58,92 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) // Runs once 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) // Runs on every event update
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) //Runs on every event update
 {
     AppState state = (AppState)appstate;
 
     return checkEvents(state, event);
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) // Superloop
+SDL_AppResult SDL_AppIterate(void *appstate) //Superloop
 {
     AppState state = (AppState)appstate;
+    
+    switch (state->gameState) {
+        case GAME_INIT:
+            SDL_Log("GAME_INIT\n");
+            state->gameState = GAME_MENY;
+            break;
 
-    switch (state->gameState)
-    {
-    case GAME_INIT:
-        SDL_Log("GAME_INIT\n");
-        state->gameState = GAME_MENY;
-        break;
+        case GAME_MENY:
+            // Meny passar bra att fixa här, oavsett vart i koden den ligger...
+            //state->gameState = GAME_TCP_INIT;
+            SDL_Log("GAME_MENY\n");
+            break;
 
-    case GAME_MENY:
-        // Meny passar bra att fixa här, oavsett vart i koden den ligger...
-        // state->gameState = GAME_TCP_INIT;
-        SDL_Log("GAME_MENY\n");
-        break;
+        case GAME_HOST:
+            //SDL_Log("GAME_HOST (waiting for IP input)\n");
+            break;
 
-    case GAME_HOST:
-        SDL_Log("GAME_HOST (waiting for IP input)\n");
-        break;
+        case GAME_JOIN:
+            //SDL_Log("GAME_JOIN (waiting for IP input)\n");
+            break;
 
-    case GAME_JOIN:
-        SDL_Log("GAME_JOIN (waiting for IP input)\n");
-        break;
+        case GAME_TCP_INIT:
+            if (initAddress(&state->serverIP, ip) < 0) {
+                createTCPClient(state->serverIP, SERVER_PORT, state);
+            }
+            state->gameState = GAME_TCP_HANDSHAKE;
+            SDL_Log("GAME_TCP_INIT\n");
+            break;
 
-    case GAME_TCP_INIT:
-        if (initAddress(&state->serverIP, ip) < 0)
-        {
-            createTCPClient(state->serverIP, SERVER_PORT, state);
-        }
-        state->gameState = GAME_TCP_HANDSHAKE;
-        SDL_Log("GAME_TCP_INIT\n");
-        break;
+        case GAME_TCP_HANDSHAKE:
+            if (checkStreamsocketConnection(state) == NET_SUCCESS) {
+                clientTCPHandshake(state);
+                state->gameState = GAME_TCP_VERIFYING_HANDSHAKE;
+            }
+            SDL_Log("GAME_TCP_HANDSHAKE\n");
+            break;
 
-    case GAME_TCP_HANDSHAKE:
-        if (checkStreamsocketConnection(state) == NET_SUCCESS)
-        {
-            clientTCPHandshake(state);
-            state->gameState = GAME_TCP_VERIFYING_HANDSHAKE;
-        }
-        SDL_Log("GAME_TCP_HANDSHAKE\n");
-        break;
-
-    case GAME_TCP_VERIFYING_HANDSHAKE:
-        if (handshakeDone(state) == NET_SUCCESS)
+        case GAME_TCP_VERIFYING_HANDSHAKE:
+            if (handshakeDone(state) == NET_SUCCESS) state->gameState = GAME_START;
+            SDL_Log("GAME_TCP_VER\n");
             state->gameState = GAME_START;
-        SDL_Log("GAME_TCP_VER\n");
-        state->gameState = GAME_START;
-        break;
+            break;
 
-    case GAME_START:
-        SDL_Log("GAME_START\n");
-        state->gameState = GAME_PLAYING;
-        break;
+        case GAME_START:
+            SDL_Log("GAME_START\n");
+            state->gameState = GAME_PLAYING;
+            break;
 
-    case GAME_PLAYING:
-        break;
+        case GAME_PLAYING:
+            break;
 
-    case GAME_OVER:
-        break;
+        case GAME_OVER:
+            break;
 
-    default:
-        break;
+        default:
+            break;
+
     }
 
     return render(state);
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) // Runs after returning APP_SUCESS or SDL_FAILURE
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) //Runs after returning APP_SUCESS or SDL_FAILURE
 {
-    if (appstate != NULL)
-    {
+    if(appstate != NULL) {
         AppState state = (AppState)appstate;
 
         destoryUDPSocket(state->udpSocket);
         stopSDLNet();
 
-        for (int x = 0; x < MAX_PLAYERS; x++)
-        {
-            if (state->players[x].texture)
-                SDL_DestroyTexture(state->players[x].texture);
+        for (int x = 0; x < MAX_PLAYERS; x++) {
+            if(state->players[x].texture) SDL_DestroyTexture(state->players[x].texture);
         }
-        if (state->renderer)
-            SDL_DestroyRenderer(state->renderer);
-        if (state->window)
-            SDL_DestroyWindow(state->window);
+        if(state->renderer) SDL_DestroyRenderer(state->renderer);
+        if(state->window) SDL_DestroyWindow(state->window);
         destroyWorld(state->world);
         SDL_free(state);
     }
